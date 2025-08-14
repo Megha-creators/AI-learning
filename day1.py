728f7676825a6a78d1c981b221ff1b44591dc544
Megha-creators <meghanathakkuri@gmail.com>
2025-08-14 17:06:02 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,1 +1,8 @@
+# Day 1 - Python Basics
+name = "Meghana"
+age = 18
+goal = "Become an AI Engineer"
 
+print("Hello, my name is", name)
+print("I am", age, "years old.")
+print("My goal is to", goal)
